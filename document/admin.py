82853a3030c6518505af679ff298606a9ccee507
osama <osamamoharm@gmail.com>
2020-08-08 05:57:46 +0200

    upgrade django  to 3.1

--- document/admin.py
+++ document/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import document,document_type,preparator_dcument
+from .models import document,document_type
 # Register your models here.
 admin.site.register(document)
 admin.site.register(document_type)
-admin.site.register(preparator_dcument)
